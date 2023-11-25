e199093bd85b7a8acd09a4c51f50323cfe03951b
Stefan Gründel <stefan.gruendel@gmail.com>
2023-11-25 12:37:22 +0100

    twMerge() implementation to use heroicons as in React

--- src/main/java/com/sgruendel/nextjs_dashboard/util/WebUtils.java
+++ src/main/java/com/sgruendel/nextjs_dashboard/util/WebUtils.java
@@ -3,6 +3,10 @@ package com.sgruendel.nextjs_dashboard.util;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
 import lombok.SneakyThrows;
+
+import java.util.List;
+import java.util.LinkedList;
+
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -37,4 +41,21 @@ public class WebUtils {
         return dto.getClass().getDeclaredField(fieldName).getAnnotation(NotNull.class) != null;
     }
 
+    public static String twMerge(final String classes, final String defaultClasses) {
+        final String[] classesSplit = classes.split(" ");
+        final List<String> utilityClasses = new LinkedList<>();
+        final String[] defaultClassesSplit = defaultClasses.split(" ");
+        String merged = classes;
+        for (int i = 0; i < classesSplit.length; i++) {
+            utilityClasses.add(classesSplit[i].split("-")[0]);
+        }
+
+        for (int i = defaultClassesSplit.length - 1; i >= 0; i--) {
+            if (!utilityClasses.contains(defaultClassesSplit[i].split("-")[0])) {
+                merged = defaultClassesSplit[i] + " " + merged;
+            }
+        }
+
+        return merged;
+    }
 }
