dee2b619ceab97c6d93cc4a866fa42de0d988914
Stefan Gründel <stefan.gruendel@gmail.com>
2023-12-17 12:06:50 +0100

    handle binding errors for create/edit invoice

--- src/main/java/com/sgruendel/nextjs_dashboard/model/InvoiceFormDTO.java
+++ src/main/java/com/sgruendel/nextjs_dashboard/model/InvoiceFormDTO.java
@@ -1,6 +1,7 @@
 package com.sgruendel.nextjs_dashboard.model;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,15 +19,19 @@ public class InvoiceFormDTO {
     @Size(max = 64)
     private String id;
 
-    @NotNull
+    @NotNull(message = "Please enter an amount.")
+    @Positive(message = "Please enter an amount greater than $0.")
     private Double amount;
 
-    @NotNull
+    @NotNull(message = "Please select an invoice status.")
     private Status status;
 
+    // no need for a NotNull message, automatically set by controller
     @NotNull
     private LocalDateTime date;
 
+    @NotNull(message = "Please select a customer.")
+    // no need for a Size message, all ids are max. 64 characters
     @Size(max = 64)
     private String customer;
 
